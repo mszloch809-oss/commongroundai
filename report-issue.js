8ba3fec89f17927535d2fb3588121e4a5220df3e
mszloch809-oss <mszloch809@gmail.com>
2026-04-21 22:28:01 -0400

    Delete report-issue.js

--- report-issue.js
+++
@@ -1,7 +0,0 @@
-import { getState } from './lib/store.js';
-import { json } from './lib/utils.js';
-
-export default async () => {
-  const state = await getState();
-  return json({ ok: true, state });
-};
